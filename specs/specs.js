18ded8055159579289f70008fe6738bbc0f83961
Brett Moore & Tanner Heffner <student@epicodus.com>
2015-04-07 12:14:17 -0700

    add second test failing

--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,7 @@ describe('anagram', function () {
     it('will return a match for letters that are the same', function() {
         expect(anagram("a", "a")).to.equal(true);
     });
+    it('will return not a match for letters that are not the same', function() {
+            expect(anagram('a', 'b')).to.equal(false);
+    });
 });
